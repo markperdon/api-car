93e6e6e9bef587dd874cfc0e72c35e167a34058c
markperdon <markperdon712@gmail.com>
2021-06-01 18:42:56 +0800

    added location

--- models/Location.php
+++ models/Location.php
@@ -1,7 +1,16 @@
 <?php
 
     class Location {
+        public function __construct() {
+        }
 
+        public function set_location() {
+
+        }
+
+        public function get_location() {
+            
+        }
     }
 
 
